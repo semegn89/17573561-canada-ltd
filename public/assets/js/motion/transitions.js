e594b9ca7434e8609b25e4220c061fe0b886b153
semegn89 <frsgroupe@gmail.com>
2025-12-30 16:46:35 +0300

    Fix enterError: use 'in' operator to check hook existence

--- public/assets/js/motion/transitions.js
+++ public/assets/js/motion/transitions.js
@@ -353,16 +353,16 @@
     })
     
     // Note: enterError hook may not be available in all Barba versions
-    // Use try-catch to safely check for enterError
-    try {
-      if (barba.hooks && barba.hooks.enterError && typeof barba.hooks.enterError === 'function') {
+    // Check if hook exists before using it
+    if (barba.hooks && 'enterError' in barba.hooks && typeof barba.hooks.enterError === 'function') {
+      try {
         barba.hooks.enterError(() => {
           Singularity.removeOverlay()
         })
+      } catch (e) {
+        // Silently ignore if hook fails
+        console.log('[Motion] enterError hook not available')
       }
-    } catch (e) {
-      // enterError not available in this version of Barba
-      console.log('[Motion] enterError hook not available')
     }
   }
   
